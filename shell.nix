1f12db0551ea414891d13a3c1375be3696cd7a35
MarcusBoay <marcusboay@hotmail.com>
2024-06-23 23:06:15 -0400

    set up rust project for nixos

---
+++ shell.nix
@@ -0,0 +1,29 @@
+/*
+based on
+https://discourse.nixos.org/t/how-can-i-set-up-my-rust-programming-environment/4501/9
+*/
+let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  rustVersion = "latest";
+  rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+    extensions = [
+      "rust-src" # for rust-analyzer
+      "rust-analyzer"
+    ];
+  };
+in
+pkgs.mkShell {
+  buildInputs = [
+    rust
+  ] ++ (with pkgs; [
+    pkg-config
+    # other dependencies
+    glib
+    gtk4
+    pango
+    wrapGAppsHook
+  ]);
+  RUST_BACKTRACE = 1;
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}
