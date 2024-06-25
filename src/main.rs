19e6020536dc4cee32b385487e23b1da9d01ac79
MarcusBoay <marcusboay@hotmail.com>
2024-06-24 20:52:35 -0400

    add mutable variable to button

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
-use std::{env, fs::File, path::Path};
+use std::{cell::Cell, env, fs::File, path::Path};
 
-use gtk4::prelude::*;
+use gtk4::{glib, prelude::*, Button, Grid};
 
-fn main() {
-    let application = gtk4::Application::builder().application_id("test").build();
-    application.connect_activate(build_ui);
-    application.run();
-}
-
-fn build_ui(application: &gtk4::Application) {
-    let window = gtk4::ApplicationWindow::new(application);
-    window.set_fullscreened(true);
-
+fn main() -> glib::ExitCode {
     // todo: process_args
 
     if !get_layout_path() {
@@ -35,6 +26,33 @@ fn build_ui(application: &gtk4::Application) {
     }
 
     // todo: get_buttons
+
+    let app = gtk4::Application::builder()
+        .application_id("rlogout")
+        .build();
+    app.connect_activate(build_ui);
+    app.run()
+}
+
+fn build_ui(app: &gtk4::Application) {
+    let window = gtk4::ApplicationWindow::new(app);
+    window.set_fullscreened(true);
+
+    // test area
+    let number = Cell::new(0);
+    let button = Button::builder()
+        .label("Oclick me!!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+    button.connect_clicked(move |_| {
+        number.set(number.get() + 1);
+        println!("clickety!! {}", number.get());
+    });
+    window.set_child(Some(&button));
+    // test area
 
     window.present();
 }
@@ -62,3 +80,7 @@ fn get_css_path() -> bool {
 
 // get_buttons
 // todo: figure out how to process jsonc
+
+fn load_buttons() {
+    let grid = Grid::new();
+}
